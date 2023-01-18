6c3b4e143b621cfdb70a3afac792a898405ad974
leebumhee <dlqjagml6447@naver.com>
2023-01-18 23:35:50 +0900

    feat : 요구사항 3 코드 작성 - POST 방식으로 회원가입 - form 형식으로 회원가입을 해야된다.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,6 +3,7 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,6 +11,7 @@ import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -30,40 +32,78 @@ public class RequestHandler extends Thread {
          */
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             BufferedReader buffer = new BufferedReader(new InputStreamReader(in,"UTF-8"));
-            Map<String, String> queryStringMap;
+
+            ArrayList<String> httpHeaders = new ArrayList<>();
             String line = buffer.readLine();
 
             if (line == null){
                 return;
             }
 
-            String[] tokens = line.split(" ");
+            while (!line.equals("")){
+                System.out.println("line = " + line);
+                httpHeaders.add(line);
+                line = buffer.readLine();
+            }
 
-            for (String token: tokens){
-                if(token.contains("?")){
-                    int idx = token.indexOf("?");
-                    String queryString = token.substring(idx + 1);
-                    queryStringMap = HttpRequestUtils.parseQueryString(queryString);
+            String[] startLine = httpHeaders.get(0).split(" ");
+            String method = startLine[0];
+            String path = startLine[1];
 
-                    String userId = queryStringMap.get("userId");
-                    String password = queryStringMap.get("password");
-                    String name = queryStringMap.get("name");
-                    String email = queryStringMap.get("email");
+            int contentLength = 0;
+            for (String httpHeader:
+                 httpHeaders) {
+                String[] header = httpHeader.split(" ");
+                if (header[0].contains("Content-Length:")){
+                    contentLength = Integer.parseInt(header[1]);
+                }
+            }
+
+            if (method.equals("GET")){
+                if (path.equals("/index.html")){
+                    byte[] body = Files.readAllBytes(new File("./webapp" + path).toPath());
+                    DataOutputStream dos = new DataOutputStream(out);
+                    response200Header(dos, body.length);
+                    responseBody(dos, body);
+                }
+
+                if (path.equals("/user/form.html")){
+                    byte[] body = Files.readAllBytes(new File("./webapp" + path).toPath());
+                    DataOutputStream dos = new DataOutputStream(out);
+                    response200Header(dos, body.length);
+                    responseBody(dos, body);
+                }
+
+                if (path.contains("?")){
+                    int idx = path.indexOf("?");
+                    String queryParam = path.substring(idx + 1);
+                    Map<String, String> queryParamMap = HttpRequestUtils.parseQueryString(queryParam);
+
+                    String userId = queryParamMap.get("userId");
+                    String password = queryParamMap.get("password");
+                    String name = queryParamMap.get("name");
+                    String email = queryParamMap.get("email");
+
+                    User user = new User(userId, password, name, email);
+                    System.out.println("user = " + user);
+                }
+            }
+
+            if (method.equals("POST")){
+                if (path.equals("/user/create")){
+                    String body = IOUtils.readData(buffer, contentLength);
+                    Map<String, String> queryParamMap = HttpRequestUtils.parseQueryString(body);
+
+                    String userId = queryParamMap.get("userId");
+                    String password = queryParamMap.get("password");
+                    String name = queryParamMap.get("name");
+                    String email = queryParamMap.get("email");
 
                     User user = new User(userId, password, name, email);
                     System.out.println("user = " + user);
 
                 }
-
-                if (token.equals("/index.html")){
-                    String url = token;
-                    byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
-                    DataOutputStream dos = new DataOutputStream(out);
-                    response200Header(dos, body.length);
-                    responseBody(dos, body);
-                }
             }
-
         } catch (IOException e) {
             log.error(e.getMessage());
         }
